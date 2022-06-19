8df85bcafb286b41ae7f15f5f64e26cfc7126b1a
lrudra <lrudra@gmail.com>
2022-06-19 00:38:33 -0500

    first test

---
+++ azure/task1/variable.tf
@@ -0,0 +1,10 @@
+variable "rgname"{
+    type = string
+    description ="used for resource group"
+}
+
+variable "rglocation"{
+    type = string
+    description = "used for selecting location"
+    default = "Central US"
+}
